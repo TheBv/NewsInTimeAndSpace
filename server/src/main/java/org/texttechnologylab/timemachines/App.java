abe4682b3f2b3ec3a49690cd7acf766d2a5c5164
Patrick Schrottenbacher <schrottenbacher@em.uni-frankfurt.de>
2024-05-21 12:21:21 +0200

    feat: added basic file logging

--- server/src/main/java/org/texttechnologylab/timemachines/App.java
+++ server/src/main/java/org/texttechnologylab/timemachines/App.java
@@ -24,7 +24,10 @@ import spark.Request;
 import spark.Response;
 import spark.servlet.SparkApplication;
 
+import java.io.IOException;
 import java.util.*;
+import java.util.logging.FileHandler;
+import java.util.logging.SimpleFormatter;
 
 import static spark.Spark.*;
 
@@ -34,12 +37,28 @@ public class App implements SparkApplication {
 
 	private static Logger logger = LoggerFactory.getLogger(App.class);
 
+	private static java.util.logging.Logger fileLogger = java.util.logging.Logger.getLogger("FileLogger");
+	private static FileHandler fh;
 	/**
 	 * Main methond to start application without webserver.
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		try {
+			// This block configure the logger with handler and formatter
+			fh = new FileHandler("./log.log");
+			fileLogger.addHandler(fh);
+			fileLogger.setUseParentHandlers(false);
+			SimpleFormatter formatter = new SimpleFormatter();
+			fh.setFormatter(formatter);
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
 		App app = new App();
 		port(8080);
 		app.init();
@@ -64,6 +83,14 @@ public class App implements SparkApplication {
 		// start downloader periodically
 		CsvDownloaderTask eventDownloader = new CsvDownloaderTask();
 		new Timer().schedule(eventDownloader, 0, 900000);
+
+		before(((request, response) -> {
+			StringBuilder sb = new StringBuilder();
+			sb.append(request.requestMethod());
+			sb.append(" | ").append(request.url());
+			sb.append(" | ").append(getParamList(request));
+			fileLogger.info(sb.toString());
+		}));
 
 		// set up generell spark response
 		after((request, response) -> {
@@ -107,5 +134,14 @@ public class App implements SparkApplication {
 		});
 
 		logger.info("finished init()");
+	}
+
+	private String getParamList(Request request) {
+		StringBuilder builder = new StringBuilder();
+
+		for (String param : request.queryParams()){
+			builder.append(param).append("=").append(String.join(",", request.queryParamsValues(param))).append(";");
+		}
+		return builder.toString();
 	}
 }
