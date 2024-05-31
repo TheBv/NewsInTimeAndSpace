5e3a0ba17352cac75b4c2bd0bac197b7a6631969
bagci <bagci@em.uni-frankfurt.de>
2024-05-31 14:54:56 +0200

    Preprocess Data with all reduce Functions

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -86,26 +86,57 @@ def run_pipeline(data_dir: str, output_dir: str, arts="PACMAP"):
         with gzip.open(out_dir, "wt", encoding="UTF-8") as json_file:
             json.dump(data, json_file, indent=2)
     fig, ax = plt.subplots(figsize=(10, 10))
+    dict_data = {}
     x_points = [i[0] for i in data["reduced_embeddings"]]
     y_points = [i[1] for i in data["reduced_embeddings"]]
     colors = ["red" if i == "Web(Patrick)" else "blue" if i == "VR(Patrick)" else "green" for i in data["labels"]]
-    ax.scatter(x_points, y_points, cmap="Spectral", label=data["labels"], c=colors)
+    shapes = ["o" if i == "Web(Patrick)" else "s" if i == "VR(Patrick)" else "^" for i in data["labels"]]
+    counter_text = 0
+    for c, label_i in enumerate(data["labels"]):
+        if label_i not in dict_data:
+            dict_data[label_i] = {
+                "x": [],
+                "y": [],
+                "color": "red" if label_i == "Web(Patrick)" else "blue" if label_i == "VR(Patrick)" else "green",
+                "shape": "o" if label_i == "Web(Patrick)" else "s" if label_i == "VR(Patrick)" else "^",
+                "counter": [],
+                "text": []
+            }
+        dict_data[label_i]["x"].append(x_points[c])
+        dict_data[label_i]["y"].append(y_points[c])
+        dict_data[label_i]["counter"].append(counter_text)
+        dict_data[label_i]["text"].append(data["text"][counter_text])
+        counter_text += 1
+    for label_i in dict_data:
+        # draw every point with different color and shape and write the number of the point
+        ax.scatter(dict_data[label_i]["x"], dict_data[label_i]["y"], cmap="Spectral", label=label_i, c=dict_data[label_i]["color"], marker=dict_data[label_i]["shape"], s=50)
+        for c, txt in enumerate(dict_data[label_i]["text"]):
+            ax.text(dict_data[label_i]["x"][c], dict_data[label_i]["y"][c], dict_data[label_i]["counter"][c])
+    # ax.scatter(x_points, y_points, cmap="Spectral", label=data["labels"], c=colors, markers=shapes)
     #Legend
     # handles, labels = ax.get_legend_handles_labels()
     # unique_labels = list(set(data["labels"]))
     # unique_labels.sort()
     # ax.legend(handles, unique_labels, title="Szenario")
-    ax.legend(title="Szenario")
-    ax.grid(True)
-    site_patch = mpatches.Patch(color='red', label='SITE')
-    nits_patch = mpatches.Patch(color='blue', label='NiTS')
-    ref_patch = mpatches.Patch(color='green', label='GPT Prompt')
-    plt.legend(handles=[site_patch, nits_patch, ref_patch])
-
-    plt.savefig(f"{output_dir}/text_{arts}.png")
+    # ax.legend(title="Szenario")
+    # ax.grid(True)
+    # site_patch = mpatches.Patch(color='red', label='SITE')
+    # nits_patch = mpatches.Patch(color='blue', label='NiTS')
+    # ref_patch = mpatches.Patch(color='green', label='GPT Prompt')
+    # plt.legend(handles=[site_patch, nits_patch, ref_patch])
+    # legend with color and markers
+    ax.legend(title="Szenario", loc="upper right")
+    # write an extra Legend outside the plot the text to the number of the point with shape and color
+    counter_text = 0
+    for label_i in dict_data:
+        for c, txt in enumerate(dict_data[label_i]["text"]):
+            ax.text(1.1, 0.9 - counter_text * 0.1, f"{dict_data[label_i]['counter'][c]} -> {txt}", transform=ax.transAxes)
+            counter_text += 0.2
+    plt.tight_layout()
+    plt.savefig(f"/storage/projects/bagci/data/NewsInTimeAndSpace/text_{arts}.png", bbox_inches = "tight")
 
 
 if __name__ == '__main__':
-    arts_reducer = ["PCA", "TSNE","PACMAP", "TRIMAP", "UMAP"]
+    arts_reducer = ["PCA", "TSNE", "PACMAP", "TRIMAP", "UMAP"]
     for art_i in arts_reducer:
         run_pipeline("Data_texts.csv", f"/storage/projects/bagci/data/NewsInTimeAndSpace/{art_i}", art_i)
